a077b3b53c2df1b354f82f85004991de7b2acf50
Yan <yan.efimov@gmail.com>
2019-04-06 16:08:03 +0300

    add pricing

--- bin/custom/tyreshopinitialdata/gensrc/com/reply/tyreshop/initialdata/constants/GeneratedTyreshopInitialDataConstants.java
+++ bin/custom/tyreshopinitialdata/gensrc/com/reply/tyreshop/initialdata/constants/GeneratedTyreshopInitialDataConstants.java
@@ -1,15 +1,8 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Mar 22, 2019 3:00:25 PM                     ---
+ * --- Generated at 31.03.2019 18:55:26                         ---
  * ----------------------------------------------------------------
- *  
- * [y] hybris Platform
- * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
- * This software is the confidential and proprietary information of SAP
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with SAP.
  */
 package com.reply.tyreshop.initialdata.constants;
 
